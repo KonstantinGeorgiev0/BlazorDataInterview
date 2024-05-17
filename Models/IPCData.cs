fb477c9c36d54300ace86703e2534e1f15fd8fac
Konstantin <konstantin.georgiev2000@gmail.com>
2024-05-17 17:11:44 +0200

    data model for the csv data

--- Models/IPCData.cs
+++ Models/IPCData.cs
@@ -4,16 +4,25 @@ namespace ItilityInterview.Models
 {
 	public class IPCData
 	{
-		[Required]
-		[Key]
 		public string IPC { get; set; }
 		public int DataFactory { get; set; }
 		public DateTime Time { get; set; }
 		public double AvgValue { get; set; }
 		public double MinValue { get; set; }
 		public double MaxValue { get; set; }
-		[Required]
 		public string MetricID { get; set; }
 		public int CpuMHz { get; set; } 
+
+		public IPCData(string ipc, int dataFactory, DateTime time, double avgValue, double minValue, double maxValue, string metricID, int cpuMHz)
+		{
+			IPC = ipc;
+			DataFactory = dataFactory;
+			Time = time;
+			AvgValue = avgValue;
+			MinValue = minValue;
+			MaxValue = maxValue;
+			MetricID = metricID;
+			CpuMHz = cpuMHz;
+		}
 	}
 }
