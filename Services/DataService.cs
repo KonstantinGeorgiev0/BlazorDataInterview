86aaa8ade28cf0f6c739e777d6ccbadcc3147716
Konstantin <konstantin.georgiev2000@gmail.com>
2024-05-17 19:58:42 +0200

    data service completed

--- Services/DataService.cs
+++ Services/DataService.cs
@@ -1,21 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
 using ItilityInterview.Models;
 
 namespace ItilityInterview.Services
 {
-    public class DataService
+    public class DataService(HttpClient httpClient)
     {
-        private readonly HttpClient _httpClient;
-
-        public DataService(HttpClient httpClient)
-        {
-            _httpClient = httpClient;
-        }
+        private readonly HttpClient _httpClient = httpClient;
 
         public async Task<List<IPCData>> LoadDataAsync(string csvFilePath)
         {
@@ -24,13 +13,13 @@ namespace ItilityInterview.Services
             try
             {
                 string csvIpcData = await _httpClient.GetStringAsync(csvFilePath);
-                List<string> dataRows = csvIpcData.Split("\n").ToList();
+                List<string> dataRows = [.. csvIpcData.Split("\n")];
 
                 for (int i = 0; i < dataRows.Count; i++)
                 {
                     if (i > 0 && !string.IsNullOrWhiteSpace(dataRows[i])) // Skip the header and empty rows
                     {
-                        List<string> ipcStringList = dataRows[i].Split(";").ToList();
+                        List<string> ipcStringList = [.. dataRows[i].Split(";")];
 
                         IPCData ipcData = new IPCData(
                             ipcStringList[0],
@@ -52,7 +41,6 @@ namespace ItilityInterview.Services
                 // handle the exception 
                 Console.WriteLine($"An error occurred while loading data: {ex.Message}");
             }
-
             return listOfIpcData;
         }
     }
