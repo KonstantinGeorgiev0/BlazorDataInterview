6f95b77840b328820035222b6b9605926b2a94fb
Konstantin <konstantin.georgiev2000@gmail.com>
2024-05-18 16:23:12 +0200

    add MudBlazor

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ItilityInterview;
 using ItilityInterview.Services; 
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,6 +12,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<DataService>();
 // Register Bootstrap
 builder.Services.AddBlazorBootstrap();
+// Register MudBlazor
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
