35e5c3cad11056be656e1cad92487e8958e0a663
Konstantin <konstantin.georgiev2000@gmail.com>
2024-05-19 14:19:38 +0200

    Use a class map to map the csv column to IPCData

---
+++ Models/IPCDataMap.cs
@@ -0,0 +1,22 @@
+using CsvHelper.Configuration;
+
+namespace BlazorInterview.Models
+/// <summary>
+/// Map the CSV columns to the IPCData properties
+/// </summary>
+{
+    public class IPCDataMap : ClassMap<IPCData>
+    {
+        public IPCDataMap()
+        {
+            Map(m => m.IPC).Name("IPC");
+            Map(m => m.DataFactory).Name("Data Factory");
+            Map(m => m.Time).Name("time").TypeConverterOption.Format("dd/MM/yyyy");
+            Map(m => m.AvgValue).Name("AvgValue");
+            Map(m => m.MinValue).Name("MinValue");
+            Map(m => m.MaxValue).Name("MaxValue");
+            Map(m => m.MetricID).Name("MetricId");
+            Map(m => m.CpuMHz).Name("CpuMHz").Convert(args => IPCData.ParseCpuMHz(args.Row.GetField("CpuMHz")));
+        }
+    }
+}
