0a80589d8824d6b68b24a76e680873a5ffff6feb
Konstantin <konstantin.georgiev2000@gmail.com>
2024-05-19 14:21:39 +0200

    Clean CpuMHz field by taking the first int value

--- Models/IPCData.cs
+++ Models/IPCData.cs
@@ -1,18 +1,27 @@
 namespace BlazorInterview.Models
 {
-    public class IPCData(string ipc, int dataFactory, DateTime time, double avgValue, double minValue, double maxValue, string metricID, int cpuMHz)
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+    public class IPCData()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
     {
-        public string IPC { get; set; } = ipc;
-        public int DataFactory { get; set; } = dataFactory;
-        public DateTime Time { get; set; } = time;
-        public double AvgValue { get; set; } = avgValue;
-        public double MinValue { get; set; } = minValue;
-        public double MaxValue { get; set; } = maxValue;
-        public string MetricID { get; set; } = metricID;
-        public int CpuMHz { get; set; } = cpuMHz;
+        public string IPC { get; set; }
+        public int DataFactory { get; set; } 
+        public DateTime Time { get; set; }
+        public double AvgValue { get; set; } 
+        public double MinValue { get; set; } 
+        public double MaxValue { get; set; } 
+        public string MetricID { get; set; } 
+        public int CpuMHz { get; set; } 
 
         // Calculated properties for utilization rates
-        public double AverageUtilizationRate => (AvgValue / CpuMHz) * 100;
-        public double PeakUtilizationRate => (MaxValue / CpuMHz) * 100;
+        public double AverageUtilizationRate => AvgValue / CpuMHz * 100;
+        public double PeakUtilizationRate => MaxValue / CpuMHz * 100;
+
+        public static int ParseCpuMHz(string cpuMHz)
+        {
+            // Clean and parse the CpuMHz field
+            var cleanValue = cpuMHz.Split(' ').FirstOrDefault(v => int.TryParse(v, out _));
+            return int.TryParse(cleanValue, out int result) ? result : 0;
+        }
     }
 }
