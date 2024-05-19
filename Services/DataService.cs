a28cb21f45a5c2b2ab8e1c2a1cd8c8d21dbb6b0c
Konstantin <konstantin.georgiev2000@gmail.com>
2024-05-19 14:22:17 +0200

    use csvHelper to get csv file data

--- Services/DataService.cs
+++ Services/DataService.cs
@@ -1,38 +1,43 @@
 using BlazorInterview.Models;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
 
 namespace BlazorInterview.Services
 {
-    public class DataService(HttpClient httpClient)
+    public class DataService
     {
-        private readonly HttpClient _httpClient = httpClient;
+        private readonly HttpClient _httpClient;
+
+        public DataService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
 
         public async Task<List<IPCData>> LoadDataAsync(string csvFilePath)
         {
-            List<IPCData> listOfIpcData = [];
-
+            List<IPCData> listOfIpcData = new List<IPCData>();
             try
             {
-                string csvIpcData = await _httpClient.GetStringAsync(csvFilePath);
-                List<string> dataRows = [.. csvIpcData.Split("\n")];
-
-                for (int i = 0; i < dataRows.Count; i++)
+                // Load the CSV data from the provided Uri and parse it into a list of IPCData objects
+                // Use get request to get the csv file
+                var csvData = await _httpClient.GetStreamAsync(csvFilePath);
+                using (var reader = new StreamReader(csvData))
                 {
-                    if (i > 0 && !string.IsNullOrWhiteSpace(dataRows[i])) // Skip the header and empty rows
+                    var config = new CsvConfiguration(CultureInfo.InvariantCulture)
                     {
-                        List<string> ipcStringList = [.. dataRows[i].Split(";")];
-
-                        IPCData ipcData = new(
-                            ipcStringList[0],
-                            int.Parse(ipcStringList[1]),
-                            DateTime.Parse(ipcStringList[2]),
-                            double.Parse(ipcStringList[3].Replace(',', '.')),
-                            double.Parse(ipcStringList[4].Replace(',', '.')),
-                            double.Parse(ipcStringList[5].Replace(',', '.')),
-                            ipcStringList[6],
-                            int.Parse(ipcStringList[7])
-                        );
-
-                        listOfIpcData.Add(ipcData);
+                        HasHeaderRecord = true,
+                        HeaderValidated = null,
+                        Delimiter = ";",
+                        IgnoreBlankLines = true,
+                        TrimOptions = TrimOptions.Trim,
+                        BadDataFound = null,
+                        PrepareHeaderForMatch = args => args.Header.ToLower(),
+                    };
+                    using (CsvReader csv = new CsvReader(reader, config))
+                    {
+                        csv.Context.RegisterClassMap<IPCDataMap>();
+                        listOfIpcData = csv.GetRecords<IPCData>().ToList();
                     }
                 }
             }
